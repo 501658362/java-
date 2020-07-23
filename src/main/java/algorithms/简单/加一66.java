b8bacf6423afe384ef1097bb114056baa688090a
chenchaopeng <501658362@qq.com>
2020-07-23 21:58:13 +0800

    leetcode 66

--- src/main/java/algorithms/简单/加一66.java
+++ src/main/java/algorithms/简单/加一66.java
@@ -25,11 +25,42 @@ package algorithms.简单;
 public class 加一66 {
 
     public static void main(String[] args) {
-
+        加一66 t = new 加一66();
+        int[] tt = new int[4];
+        tt[0] = 9;
+        tt[1] = 9;
+        tt[2] = 9;
+        tt[3] = 9;
+        int[] ints = t.plusOne(tt);
+        System.out.println(1);
     }
 
     public int[] plusOne(int[] digits) {
-
-        return null;
+        int add = 1;
+        int temp = 0;
+        for (int i = digits.length - 1; i >= 0; i--) {
+            if (add > 0) {
+                int result = digits[i] + add;
+                if (result >= 10) {
+                    add = 1;
+                    digits[i] = result % 10;
+                    if (i == 0) {
+                        temp = 1;
+                    }
+                } else {
+                    add = 0;
+                    digits[i] = result;
+                }
+            } else {
+                break;
+            }
+        }
+        if (temp > 0) {
+            int[] result = new int[digits.length + 1];
+            result[0] = temp;
+            System.arraycopy(digits, 0, result, 1, digits.length);
+            return result;
+        }
+        return digits;
     }
 }
