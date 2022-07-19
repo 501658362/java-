6c12e3f6e915b0d2cc12bfac1146bb78663d2f30
chenchaopeng <501658362@qq.com>
2022-07-19 16:28:43 +0800

    提供基类

---
+++ src/main/java/algorithms/ListNode.java
@@ -0,0 +1,24 @@
+package algorithms;
+
+/**
+ * Created by IntelliJ IDEA.
+ *
+ * @author: chenchaopeng Date: 2022/7/19
+ */
+public class ListNode {
+
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
